e157fbbc00e9a91e3af9fb845a62abf1b982143e
Karthick Ganesh S B <karthickganesh1101@gmail.com>
2025-02-22 23:26:19 +0530

    Fack

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,7 +27,7 @@ const HomePage = () => {
     const lenis = new Lenis({
       duration: 1.2,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-      smooth: true
+      // smooth: true
     });
 
     // Integrate Lenis with GSAP
